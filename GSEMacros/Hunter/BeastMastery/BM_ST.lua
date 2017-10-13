f9e42e3783b927d9d7ff8e8839195de5da7c6d71
Jabberie <jabberie@gmail.com>
2017-10-13 20:43:10 +0100

    Update

--- GSEMacros/Hunter/BeastMastery/BM_ST.lua
+++ GSEMacros/Hunter/BeastMastery/BM_ST.lua
@@ -1,8 +1,12 @@
 Sequences['BM_ST'] = {
-  Author="Jabberie@draka",  
+  Author="Jabberie@draka",
   SpecID=253,
-  Talents = "3112333",
+  Talents = "3,1,1,2,3,3,3",
   Default=1,
+  Raid=2,
+  Dungeon=2,
+  Heroic=2,
+  Mythic=2,
   Icon='ability_hunter_bestialdiscipline',
   MacroVersions = {
     [1] = {
@@ -20,12 +24,38 @@ Sequences['BM_ST'] = {
         "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
         "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
         "/castsequence [combat] Dire Beast, Titan's Thunder",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",        
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
       PostMacro={
       },
       KeyRelease={
         "/cast [combat] Bestial Wrath",
         "/cast [combat] Blood Fury",
+        "/cast [combat] Berserking",
+        "/cast [combat] Aspect of the Wild",
+      },
+    },
+    [2] = {
+      StepFunction = "Sequential",
+      KeyPress={
+        "/cast [@focus, exists, nodead][@party1, exists, nodead][@pet, exists, nodead] Misdirection",
+        "/petautocastoff [group] Growl",
+        "/petautocaston [nogroup] growl",
+      },
+      PreMacro={
+      },
+        "/cast [nopet][@pet,dead] Revive Pet",
+        "/cast Dire Beast",
+        "/cast [nopet] Call Pet 1",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+        "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
+        "/castsequence [combat] Dire Beast, Titan's Thunder",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+      PostMacro={
+      },
+      KeyRelease={
+        "/cast [combat] Bestial Wrath",
+        "/cast [combat] Blood Fury",
+        "/cast [combat] Berserking",
         "/cast [combat] Aspect of the Wild",
       },
     },
