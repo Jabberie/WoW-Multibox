2362e96fb98828721ed22fc1ab4508ecfa236581
Jabberie <jabberie@gmail.com>
2018-07-19 10:29:19 +0100

    no message

--- GSEMacros/Legion/Hunter/BeastMastery/BM_ST.lua
+++ GSEMacros/Legion/Hunter/BeastMastery/BM_ST.lua
@@ -1,7 +1,7 @@
 Sequences['BM_ST'] = {
   Author="Jabberie@draka",
   SpecID=253,
-  Talents = "3,1,1,2,3,3,3",
+  Talents = "2,1,1,1,?,1,3",
   Default=1,
   Raid=2,
   Dungeon=2,
@@ -21,10 +21,10 @@ Sequences['BM_ST'] = {
         "/cast [nopet][@pet,dead] Revive Pet",
         "/cast Dire Beast",
         "/cast [nopet] Call Pet 1",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot, Multi-Shot",
         "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
         "/castsequence [combat] Dire Beast, Titan's Thunder",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot, Multi-Shot",
       PostMacro={
       },
       KeyRelease={
@@ -46,10 +46,10 @@ Sequences['BM_ST'] = {
         "/cast [nopet][@pet,dead] Revive Pet",
         "/cast Dire Beast",
         "/cast [nopet] Call Pet 1",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot, Cobra Shot",
         "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
         "/castsequence [combat] Dire Beast, Titan's Thunder",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot, Cobra Shot",
       PostMacro={
       },
       KeyRelease={
