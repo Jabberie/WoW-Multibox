cdc65250575d483a7924b4c15c039f389437310e
Jabberie <jabberie@gmail.com>
2018-09-08 21:20:26 +0100

    no message

--- GSEMacros/BFA/Death Knight/ground_macros.lua
+++ GSEMacros/BFA/Death Knight/ground_macros.lua
@@ -1,4 +1,4 @@
-#showtooltip  Death and Decay
-/cast [button:2]  Death and Decay
-/stopmacro [button:2]  Death and Decay
-/cast [@player,combat]  Death and Decay
+#showtooltip Death and Decay
+/cast [button:2] Death and Decay
+/stopmacro [button:2]
+/cast [@player,combat] Death and Decay
